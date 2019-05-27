e3e60db12469d380126cee293caa46334c653ba9
Jeremy <yukho113@gmail.com>
2019-05-27 22:15:40 +1000

    Implemented picking image from Gallery as requested in #15

--- Xcode/Receipt Master/Receipt Master/NewReceiptViewController.swift
+++ Xcode/Receipt Master/Receipt Master/NewReceiptViewController.swift
@@ -11,9 +11,6 @@ import MapKit
 import CoreLocation
 
 class NewReceiptViewController: UIViewController, CLLocationManagerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    @IBOutlet weak var cameraView: UIView!
-    //Contain image from camera in this view later
-    
     @IBOutlet weak var photoView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
@@ -136,6 +133,13 @@ class NewReceiptViewController: UIViewController, CLLocationManagerDelegate, UIN
         print(latitude)
     }
     
+    @IBAction func chooseFromGalleryButtonPressed(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     /*
      // MARK: - Navigation
      
